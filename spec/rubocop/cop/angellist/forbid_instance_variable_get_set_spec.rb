0272a81e4694ec5ebbc236296406ee1e9aa1db1e
Dan Cetlin <dan.cetlin@angellist.com>
2025-09-19 11:17:20 -0700

    fix failing testfile

--- spec/rubocop/cop/angellist/forbid_instance_variable_get_set_spec.rb
+++ spec/rubocop/cop/angellist/forbid_instance_variable_get_set_spec.rb
@@ -2,19 +2,19 @@
 # frozen_string_literal: true
 
 RSpec.describe RuboCop::Cop::Angellist::ForbidInstanceVariableGetSet, :config do
-  let(:config) { RuboCop::Config.new }
+  let(:config) { RuboCop::Config.new('Angellist/ForbidInstanceVariableGetSet' => { 'Enabled' => true }) }
 
   it 'registers an offense when using instance_variable_get' do
     expect_offense(<<~RUBY)
       object.instance_variable_get(:@foo)
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Angellist/ForbidInstanceVariableGetSet: Avoid using `instance_variable_get` or `instance_variable_set`. Prefer using public getters/setters.
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Avoid using `instance_variable_get` or `instance_variable_set`. Prefer using public getters/setters.
     RUBY
   end
 
   it 'registers an offense when using instance_variable_set' do
     expect_offense(<<~RUBY)
       object.instance_variable_set(:@foo, value)
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Angellist/ForbidInstanceVariableGetSet: Avoid using `instance_variable_get` or `instance_variable_set`. Prefer using public getters/setters.
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Avoid using `instance_variable_get` or `instance_variable_set`. Prefer using public getters/setters.
     RUBY
   end
 
@@ -33,14 +33,14 @@ RSpec.describe RuboCop::Cop::Angellist::ForbidInstanceVariableGetSet, :config do
   it 'registers an offense when using instance_variable_get with string argument' do
     expect_offense(<<~RUBY)
       object.instance_variable_get("@foo")
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Angellist/ForbidInstanceVariableGetSet: Avoid using `instance_variable_get` or `instance_variable_set`. Prefer using public getters/setters.
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Avoid using `instance_variable_get` or `instance_variable_set`. Prefer using public getters/setters.
     RUBY
   end
 
   it 'registers an offense when using instance_variable_set with string argument' do
     expect_offense(<<~RUBY)
       object.instance_variable_set("@foo", value)
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Angellist/ForbidInstanceVariableGetSet: Avoid using `instance_variable_get` or `instance_variable_set`. Prefer using public getters/setters.
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Avoid using `instance_variable_get` or `instance_variable_set`. Prefer using public getters/setters.
     RUBY
   end
 end
